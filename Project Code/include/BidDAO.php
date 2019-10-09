6eca2ed25dfea3e314a55ddc28e692c0c4036e4c
Ashley LAU Bing Heng <ashley.lau.2018@smu.edu.sg>
2019-10-09 22:10:20 +0800

    Update bidding processing codes in BidDAO.php

--- Project Code/include/BidDAO.php
+++ Project Code/include/BidDAO.php
@@ -58,7 +58,23 @@ class BidDAO {
         }
         return $studentBid; 
     }
-	
+    
+    public function checkForSimilarCourseBid($student,$courseid){
+        $sql = 'select * from bid where userid=:userid and courseid=:courseid';
+        
+        $connMgr = new ConnectionManager();
+        $conn = $connMgr->getConnection();
+        
+        $stmt = $conn->prepare($sql);
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $stmt->bindParam(':userid', $student, PDO::PARAM_STR);
+        $stmt->bindParam(':courseid', $courseid, PDO::PARAM_STR);
+        $stmt->execute();
+        
+        return $stmt->rowCount();
+    
+    }
+
 	 public function removeAll() {
         $sql = 'TRUNCATE TABLE bid';
         
@@ -70,6 +86,40 @@ class BidDAO {
         $stmt->execute();
         $count = $stmt->rowCount();
     }    
+
+    public function processBID($sectionBids, $courseid, $section, $round) {
+        #obtain class size - courseid and section
+
+        $sql = "SELECT SIZE FROM SECTION WHERE COURSEID = :COURSEID AND SECTIONID = :SECTIONID";
+
+        $connMgr = new ConnectionManager();      
+        $conn = $connMgr->getConnection();
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindParam(':COURSEID', $courseid, PDO::PARAM_STR);
+        $stmt->bindParam(':SECTION', $section, PDO::PARAM_STR);
+        $stmt->execute();
+
+        $row = $stmt->fetch();
+        $size = $row['SIZE'];
+
+        #n = size
+        if( sizeof($sectionBids) <= $size ){
+            #update student bids to all success
+        }
+        else{
+            $clearingPrice = $sectionBids[$size-1]->amount;
+            $rejectPrice = $sectionBids[$size]->amount;
+
+            if ($clearingPrice == $rejectPrice){
+                #run checking code to see if amount = clearing price in order to drop bid
+            }
+            else{
+                #n+1 onwards all failed bid
+            }
+        }
+
+    }
 	
 }
 
